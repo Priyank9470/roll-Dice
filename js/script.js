58da7ef0081aef8a58a400fa2d591748dcf3062e
Priyank9470 <112840380+Priyank9470@users.noreply.github.com>
2024-05-05 15:34:42 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,13 @@
+
+let numberArray = []
+function rollDice() {
+    var limit = document.getElementById("numberOfDice").value
+    let generatedNumber = Math.floor(Math.random() * limit) + 1
+    if (numberArray.includes(generatedNumber) === false) {
+        numberArray.push(generatedNumber)
+        document.getElementById("generated-number").textContent = `${generatedNumber}`;
+    }
+    else if (numberArray.length == limit) {
+        alert("Limit reached")
+    }
+}
